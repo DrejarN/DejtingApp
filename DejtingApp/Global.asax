efd8ec212d9e0bd823d08c2a99897c4c2131dbab
Jamie Jones <30621704+DrejarN@users.noreply.github.com>
2019-12-19 13:25:18 +0100

    Add project files.

---
+++ DejtingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DejtingApp.MvcApplication" Language="C#" %>
---
+++ DejtingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DejtingApp.Startup))]
+namespace DejtingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

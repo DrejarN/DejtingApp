4e2b2d0c8147802ceb60f060b91628440c3f291b
th0rnie <46375959+th0rnie@users.noreply.github.com>
2020-01-07 12:14:57 +0100

    amanda fuckar upp :(
    <Compile Include="Models\ExampleUser.cs" />
    
    ÄR DET ENDA SOM LAGTS TILL I DEJTINGAPP.CSPROJ

---
+++ DejtingApp/Models/ExampleUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DejtingApp.Models
+{
+    public class ExampleUser
+    {
+        
+        [Display(Name = "Bild")]
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Namn")]
+        public string Förnamn { get; set; }
+
+        [Display(Name = "Ålder")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Födelseår { get; set; }
+
+    }
+}
